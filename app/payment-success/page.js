20a50c87f523eb9e590807968319a574c33bfa5b
Vincent Marmion <vintsmar@gmail.com>
2026-05-13 13:42:14 +0100

    Fix eslint warning in payment-success

--- app/payment-success/page.js
+++ app/payment-success/page.js
@@ -1,5 +1,5 @@
 'use client';
-
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState, Suspense } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 
